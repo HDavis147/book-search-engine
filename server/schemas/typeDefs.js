0138553d98ea911e032bb2de3724628db8446912
Houston Davis <133936381+HDavis147@users.noreply.github.com>
2023-11-13 14:40:54 -0600

    added necessary types to typeDefs. Need mutations.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,29 @@
 const typeDefs = `#graphql 
+  type Query: {
+    me: User
+  }
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
